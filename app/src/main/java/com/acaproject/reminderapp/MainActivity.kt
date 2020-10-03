1d7179cfdbaaa64ed100089d2078616a492aad4a
taron-gh <ghlijyantaron@gmail.com>
2020-10-03 13:30:04 +0400

    Added toast

--- app/src/main/java/com/acaproject/reminderapp/MainActivity.kt
+++ app/src/main/java/com/acaproject/reminderapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.acaproject.reminderapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Message", Toast.LENGTH_SHORT).show()
     }
 }
