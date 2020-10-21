f00382fbefef92c9563ae2790e084d74b68f0913
taron-gh <ghlijyantaron@gmail.com>
2020-10-21 12:58:00 +0400

    Created NotificationReceiver.kt broadcast receiver

--- app/src/main/java/com/acaproject/reminderapp/MainActivity.kt
+++ app/src/main/java/com/acaproject/reminderapp/MainActivity.kt
@@ -15,20 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        createNotificationChannel()
+
     }
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                CHANNEL_DEFAULT,
-                "Service channel",
-                NotificationManager.IMPORTANCE_DEFAULT
-            ).apply {
-                description = "Channel for foreground services"
-            }
-            val notificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
+
 }
--- app/src/main/java/com/acaproject/reminderapp/ReminderApplication.kt
+++ app/src/main/java/com/acaproject/reminderapp/ReminderApplication.kt
@@ -1,10 +1,26 @@
 package com.acaproject.reminderapp
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 
 class ReminderApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         TaskManager.init(this)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_DEFAULT,
+                "Service channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                description = "Channel for foreground services"
+            }
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
