bf0d104805a47afd0b75a29ffac971387d833285
taron-gh <ghlijyantaron@gmail.com>
2020-11-04 20:08:11 +0400

    cleared master branch

--- app/src/main/java/com/acaproject/reminderapp/Database.kt
+++
@@ -1,9 +0,0 @@
-package com.acaproject.reminderapp
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [Task::class], version = 1)
-abstract class Database : RoomDatabase() {
-    abstract fun tasksDao(): TaskDao
-}
